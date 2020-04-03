c432023a459b8d7ad496d8a4b44eda3dfba54fbc
Pace Davis <pacemdavis@gmail.com>
2020-04-02 21:02:59 -0500

    woot

--- music-app/src/components/Dashboard.js
+++ music-app/src/components/Dashboard.js
@@ -12,9 +12,9 @@ import SoundQuality from './SoundQuality'
 
 
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+  // constructor(props) {
+  //   super(props)
+    state = {
       online: true,
       volume: 30,
       quality: '20',
@@ -22,31 +22,59 @@ class Dashboard extends React.Component {
       message: ''
     }
     // this.listAdd = this.listAdd.bind(this)
-    this.switchToggle = this.switchToggle.bind(this)
-  }
+    // this.switchToggle = this.switchToggle.bind(this)
+  // }
 
 
   switchToggle = event => {
-    event.preventDefault()
+    const message = "Your application is offline. You won't be able to share or stream music to other devices. if switch is turned offline."   
+    const newOnline = !this.state.online
     this.setState({
-      online: !this.state.online
+      online: newOnline
     })
+  
+    if(newOnline === false) {
+      this.setState({
+        notifications: [...this.state.notifications, message]
+      })
+    }
+    console.log(newOnline)
   }
 
-  volumeToggle = event => {
-    event.preventDefault()
-    let tempObject = {[event.target.name]:Number(event.target.value)}
+  volumeToggle = (event, newValue) => {
+    const message = "Listening to music at a high volume could cause long-term hearing loss."
     this.setState({
-      volume: Number(event.target.value)
+      volume: newValue,
     })
+    if(newValue > 80) {
+      this.setState({
+        notifications: [...this.state.notifications, message]
+      })
+    }
+  
   }
 
-  notificationUpdate = event => {
-    event.preventDefault()
+  qualityToggle = (event) => {
+    const message = "Music quality is degraded. Increase quality if your connection allows it."
     this.setState({
-      notifications: [...this.state.notifications, this.state.message]
+      quality: event.target.value
     })
+    
+  
+    if(event.target.value === 10) {
+      this.setState({
+        notifications: [...this.state.notifications, message]
+      })
+    }
+  
   }
+
+  // notificationUpdate = event => {
+  //   event.preventDefault()
+  //   this.setState({
+  //     notifications: [...this.state.notifications, this.state.message]
+  //   })
+  // }
 
   render() {
     // const notifications = this.state.notifications;
@@ -61,13 +89,16 @@ class Dashboard extends React.Component {
             <MusicVolume onChange ={this.volumeToggle} volume ={this.state.volume} />
           </div>
           <div className="card">
-            <SoundQuality />
+            <SoundQuality onChange={this.qualityToggle} quality={this.state.quality}/>
           </div>
         </div>
         <br/>
         <div>
           <h2>System Notifications:</h2>
-          {notifications}
+            <div>
+              {this.state.notifications}
+            </div>
+          {/* {notifications} */}
         </div>
       </div>
     )
--- music-app/src/components/MusicVolume.js
+++ music-app/src/components/MusicVolume.js
@@ -10,6 +10,15 @@ import Slider from '@material-ui/core/Slider';
 
 const musicVolume = (props) => {
   console.log(props)
+  // const tooLoud = () => {
+  //   if(props.volume > 80) {
+  //     return (
+  //       <ul>Listening to music at a high volume could cause long-term hearing loss.</ul>
+  //     )
+  //   } else {
+  //     return props.volume
+  //   }
+  // }
   return (
     <div>
       <Card>
